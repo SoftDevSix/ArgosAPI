ef517a1769e1007b3ce4fc18a080c07bd80790bf
gabygabylagaby <gabrielagv7656@gmail.com>
2024-11-17 20:32:37 -0400

    feat: initialization of the rest api of argos

---
+++ apirest/src/main/java/com/argos/apirest/TestApi.java
@@ -0,0 +1,16 @@
+package com.argos.apirest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class TestApi {
+
+    @GetMapping("/{name}")
+    public String test(@PathVariable String name) {
+        return "Hello World - " + name;
+    }
+}
