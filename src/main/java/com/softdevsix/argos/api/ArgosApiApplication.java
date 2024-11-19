3d064307aa7dcef7e6b9782cd552bda9a5768663
Terán Rocha José Luis <joseluisteran2617@gmail.com>
2024-11-19 11:50:49 -0400

    refactor: update project setup for better configuration

---
+++ src/main/java/com/softdevsix/argos/api/ArgosApiApplication.java
@@ -0,0 +1,13 @@
+package com.softdevsix.argos.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArgosApiApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ArgosApiApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/softdevsix/argos/api/ArgosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softdevsix.argos.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArgosApiApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
