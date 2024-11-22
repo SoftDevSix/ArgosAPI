c4e9fd4ac389541dc95001ffd71991bfc7d4c0ab
gabygabylagaby <gabrielagv7656@gmail.com>
2024-11-21 21:00:00 -0400

    feat: add entities for coverage data

---
+++ src/main/java/com/softdevsix/api/Entity/ClassData.java
@@ -0,0 +1,25 @@
+package com.softdevsix.api.Entity;
+
+import lombok.Data;
+import java.util.List;
+
+/**
+ * Represents coverage data for a specific class.
+ */
+@Data
+public class ClassData {
+    /**
+     * Name of the class.
+     */
+    private String className;
+
+    /**
+     * Path to the class file.
+     */
+    private String pathClass;
+
+    /**
+     * List of methods with their coverage data.
+     */
+    private List<MethodData> methods;
+}
---
+++ src/main/java/com/softdevsix/api/Entity/MethodData.java
@@ -0,0 +1,30 @@
+package com.softdevsix.api.Entity;
+
+import lombok.Data;
+import java.util.List;
+
+/**
+ * Represents method-level coverage data.
+ */
+@Data
+public class MethodData {
+    /**
+     * Name of the method.
+     */
+    private String name;
+
+    /**
+     * Description of the method.
+     */
+    private String desc;
+
+    /**
+     * List of all statements in the method.
+     */
+    private List<Integer> allStatements;
+
+    /**
+     * List of statements covered by the test in the method.
+     */
+    private List<Integer> coveragedStatements;
+}
