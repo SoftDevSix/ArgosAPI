ec55fb2dc6ec8ae57701ae1b9fe7df527c74e346
Terán Rocha José Luis <joseluisteran2617@gmail.com>
2024-11-27 15:18:04 -0400

    chore: set initial project structure

---
+++ src/test/java/com/softdevsix/ArgosApplicationTest.java
@@ -0,0 +1,13 @@
+package com.softdevsix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArgosApplicationTest {
+
+//  @Test
+//  void contextLoads() {
+//  }
+
+}
