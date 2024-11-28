977a188dff3a5a7536066f7e7720a37f820f47e9
unknown <deluxcero@gmail.com>
2024-11-27 22:42:56 -0400

    fix: resolve sonarqube in coverage

---
+++ src/main/test/com/softdevsix/api/ArgosApplicationTests.java
@@ -0,0 +1,19 @@
+package com.softdevsix.api;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootTest
+class ArgosApplicationTests {
+
+    @Autowired
+    private ApplicationContext context;
+
+    @Test
+    void contextLoads() {
+        assertNotNull(context, "The application context should have loaded.");
+    }
+}
