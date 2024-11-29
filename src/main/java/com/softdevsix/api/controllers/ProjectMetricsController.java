c358627226c7aa9c1cad2d6e62fbcdfd573dc28f
Leonardo Herrera <leonardo.herrera@fundacion-jala.org>
2024-11-28 23:20:07 -0400

    fix: endpoint mapping changed

--- src/main/java/com/softdevsix/api/controllers/ProjectMetricsController.java
+++ src/main/java/com/softdevsix/api/controllers/ProjectMetricsController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/metrics")
+@RequestMapping("/coverage")
 public class ProjectMetricsController {
     private final IProjectService projectService;
 
