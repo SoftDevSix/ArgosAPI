f448132f9bc142640b237652ed6dc04233643e1d
Santiago C. Saavedra <zrock_git@zohomail.com>
2024-11-29 07:12:55 -0400

    refactor: change name of request

--- src/main/java/com/softdevsix/api/controllers/CoverageNotificationController.java
+++ src/main/java/com/softdevsix/api/controllers/CoverageNotificationController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/coverageNotification")
+@RequestMapping("/coverage/create-notification")
 public class CoverageNotificationController {
 
     private final IFileManagerClientRepository fileManagerClient;
