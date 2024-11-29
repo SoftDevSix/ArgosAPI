50ce5d46265f188fc254d45ba0e86031600e7bff
Santiago C. Saavedra <zrock_git@zohomail.com>
2024-11-29 03:23:18 -0400

    chore: save coverage report in memorydb
    - Implement saveReportToDatabase method

--- src/main/java/com/softdevsix/api/repositories/IProjectRepository.java
+++ src/main/java/com/softdevsix/api/repositories/IProjectRepository.java
@@ -4,6 +4,7 @@ package com.softdevsix.api.repositories;
 import com.softdevsix.api.domain.entities.project.Project;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
@@ -11,4 +12,6 @@ public interface IProjectRepository {
     Project findById(UUID id);
 
     void save(Project project);
+
+    List<Project> getAll();
 }
