81712a2742599f3e4928af30fff952f026040ac3
Leonardo Herrera <leonardo.herrera@fundacion-jala.org>
2024-12-03 00:01:44 -0400

    feat: error handling in the controller and service

--- src/main/java/com/softdevsix/application/services/Rules/RulesService.java
+++ src/main/java/com/softdevsix/application/services/Rules/RulesService.java
@@ -3,6 +3,7 @@ package com.softdevsix.application.services.Rules;
 import com.softdevsix.application.services.Project.IProjectService;
 import com.softdevsix.domain.entities.project.Project;
 import com.softdevsix.domain.entities.project.ProjectParams;
+import com.softdevsix.domain.exceptions.BadRequestException;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
@@ -17,6 +18,8 @@ public class RulesService implements IRulesService {
 
     @Override
     public void saveRules(ProjectParams projectParams, UUID projectId) {
+        if(projectParams == null) throw new BadRequestException("Project params cannot be null");
+
         Project project = PROJECT_SERVICE.getProjectById(projectId);
 
         project.setProjectParams(projectParams);
