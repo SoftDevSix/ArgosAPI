e88da640e368e953e177ca78bdbdaf6a8905d0ac
gabygabylagaby <gabrielagv7656@gmail.com>
2024-12-03 23:36:32 -0400

    fix: fixed test for controller

--- src/test/java/com/softdevsix/application/services/report/ReportServiceTest.java
+++ src/test/java/com/softdevsix/application/services/report/ReportServiceTest.java
@@ -21,6 +21,6 @@ class ReportServiceTest {
         ReportService reportService = new ReportService(null, projectRepository, fileRepository);
         reportService.saveReportToDatabase("6570409c-44d0-4ca5-b271-fe433a0a290a", report);
         assertEquals(1, projectRepository.getAll().size());
-        assertEquals(2+3, fileRepository.getAll().size());
+        assertEquals(2, fileRepository.getAll().size());
     }
 }
