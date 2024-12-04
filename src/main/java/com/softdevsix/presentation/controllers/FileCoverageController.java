611f471732f8d0adb3c5434597171e6dde9928fb
gabygabylagaby <gabrielagv7656@gmail.com>
2024-12-03 23:59:00 -0400

    fix: add exceptions not found file

--- src/main/java/com/softdevsix/presentation/controllers/FileCoverageController.java
+++ src/main/java/com/softdevsix/presentation/controllers/FileCoverageController.java
@@ -5,6 +5,7 @@ import com.softdevsix.application.services.IProjectService;
 import com.softdevsix.domain.entities.file.File;
 import com.softdevsix.application.dto.FileCoverageDto;
 import com.softdevsix.domain.entities.project.Project;
+import com.softdevsix.domain.exceptions.ProjectNotFoundException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.UUID;
@@ -31,7 +32,7 @@ public class FileCoverageController {
     public ResponseEntity<FileCoverageDto> getFileCoverage(@PathVariable UUID idProject, @RequestParam String filePath) {
         Project project = projectService.getProjectById(idProject);
 
-        UUID fileId = project.getCoveredFiles().stream().filter(f -> filePath.contains(f.getPath())).findFirst().orElseThrow(() -> new IllegalArgumentException("File not found"))
+        UUID fileId = project.getCoveredFiles().stream().filter(f -> filePath.contains(f.getPath())).findFirst().orElseThrow(() -> new ProjectNotFoundException("File not found"))
                 .getFileId();
         File file = fileService.getFileById(fileId);
 
