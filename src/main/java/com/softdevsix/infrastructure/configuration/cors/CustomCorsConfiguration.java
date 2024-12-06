f2dfbda98ed414eced26f9f24664ed2e9ff3d728
Santiago C. Saavedra <zrock_git@zohomail.com>
2024-12-06 13:46:34 -0400

    fix: implement CORS configuration

--- src/main/java/com/softdevsix/infrastructure/configuration/cors/CustomCorsConfiguration.java
+++ src/main/java/com/softdevsix/infrastructure/configuration/cors/CustomCorsConfiguration.java
@@ -12,7 +12,7 @@ public class CustomCorsConfiguration implements CorsConfigurationSource {
     @Override
     public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173"));
+        config.setAllowedOrigins(List.of(System.getenv("ArgosUI_address"), System.getenv("ArgosFileManager_address")));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
         config.setAllowedHeaders(List.of("*"));
         return config;
